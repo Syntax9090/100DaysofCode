928f86b47f7ecce1fb8b050c98d14c6aee86451a
Syntax <118423998+Syntax9090@users.noreply.github.com>
2024-06-13 00:56:48 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,25 @@
+# Welcome to my first Python GitHub project!
+# Get ready to laugh and enjoy the world of coding with a touch of humor.
+# This script will not only greet you but also help you come up with an awesome band name.
+# Because let's be honest, every coder secretly dreams of becoming a rockstar, right? 😎🎸
+
+# Prepare yourself for the Band Name Generator - the only tool you'll need to achieve your musical dreams!
+# Just kidding, you'll still need talent, instruments, and a band... but hey, at least you'll have a cool name!
+
+# Start your engines... I mean, start your code!
+
+# Print a greeting message that includes the user's name
+# The input function prompts the user to enter their name, which is then concatenated with "Hello " and "!"
+print("Hello " + input("What is your name?") + "!")
+
+# Welcome message for the Band Name Generator
+print("Welcome to the Band Name Generator.")
+
+# Prompt the user to enter the name of the city they grew up in and store it in the variable 'street'
+street = input("What's the name of the city you grew up in?\n")
+
+# Prompt the user to enter their pet's name and store it in the variable 'pet'
+pet = input("What's your pet's name?\n")
+
+# Combine the values of 'street' and 'pet' to generate a band name and print it
+print("Your band name could be " + street + " " + pet)
